749fa7afb4a3260ca0c1215c07a88c0075f9f957
marappoli <marappoli@outlook.com>
2024-04-29 20:28:27 -0300

    Inserção do Tigrinho em todas as cenas

--- Assets/Scripts/Tigrinho/PlayerMovement.cs
+++ Assets/Scripts/Tigrinho/PlayerMovement.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
- 
+
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
@@ -21,7 +21,15 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(6, 6, 1);
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-6, 6, 1);
-  
     }
 
+    // This function is called when the Collider other enters the trigger.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            // If the player collides with a wall, stop its movement.
+            body.velocity = Vector2.zero;
+        }
+    }
 }
